0cdc5abd93381e36013b7c9ec50e0f8b2443342d
Chameleon <chameleon@Xubuntu.myguest.virtualbox.org>
2023-07-20 13:37:19 +0300

    added Computer Choice Randomizer

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice()
+{
+    const choices = ["Rock", "Paper" , "Scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+} 
+
+const randomChoice = getComputerChoice();
+console.log(randomChoice);
