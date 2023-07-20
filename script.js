b42b82db24d47ab0a38fa9498d812eb6c4b07e1a
Chameleon <chameleon@Xubuntu.myguest.virtualbox.org>
2023-07-20 15:57:04 +0300

    Added a round of 5

--- script.js
+++ script.js
@@ -16,46 +16,78 @@ function getPlayerChoice()
     }
 }
 
-function playRound(playerSelection, computerSelection)
+
+function game()
 {
-    if (playerSelection === computerSelection)
+    let playerCounter = 0;
+    let computerCounter = 0;
+
+    for (let i = 0; i < 5; i++)
+    {      
+        function playRound(playerSelection, computerSelection)
+        {
+            if (playerSelection === computerSelection)
+            {
+                console.log("It's a tie!");
+            }
+            else if (playerSelection !== computerSelection)
+            {
+                if (playerSelection === "rock" && computerSelection === "paper")
+                {
+                    console.log("The Computer wins! Paper beats Rock!");
+                    computerCounter++;
+                }
+                else if (playerSelection === "rock" && computerSelection === "scissors")
+                {
+                    console.log("The Player wins! Rock beats Scissors!");
+                    playerCounter++;
+                }
+                else if (playerSelection === "paper" && computerSelection === "rock")
+                {
+                    console.log("The Player wins! Paper beats Rock!");
+                    playerCounter++;
+                }
+                else if (playerSelection === "paper" && computerSelection === "scissors")
+                {
+                    console.log("The Computer wins! Scissors beats Paper!");
+                    computerCounter++;
+                }
+                else if (playerSelection === "scissors" && computerSelection === "rock")
+                {
+                    console.log("The Computer wins! Rock beats Scissors!");
+                    computerCounter++;
+                }
+                else
+                {
+                    console.log("The Player wins! Scissors beats Paper!");
+                    playerCounter++;
+                }
+                
+            }
+
+        }
+
+        console.log(playRound(playerSelection, computerSelection));
+        const playerSelection = getPlayerChoice();
+        console.log(playerSelection);
+        const computerSelection = getComputerChoice();
+        console.log(computerSelection);
+    }
+
+    if (playerCounter > computerCounter)
+    {
+        console.log("Te player wins!");
+    }
+    else if (playerCounter < computerCounter)
+    {
+        console.log("Te computer wins!");
+    }
+    else
     {
         console.log("It's a tie!");
     }
-    else if (playerSelection !== computerSelection)
-    {
-        if (playerSelection === "rock" && computerSelection === "paper")
-        {
-            console.log("The Computer wins! Paper beats Rock!");
-        }
-        else if (playerSelection === "rock" && computerSelection === "scissors")
-        {
-            console.log("The Player wins! Rock beats Scissors!");
-        }
-        else if (playerSelection === "paper" && computerSelection === "rock")
-        {
-            console.log("The Player wins! Paper beats Rock!");
-        }
-        else if (playerSelection === "paper" && computerSelection === "scissors")
-        {
-            console.log("The Computer wins! Scissors beats Paper!");
-        }
-        else if (playerSelection === "scissors" && computerSelection === "rock")
-        {
-            console.log("The Computer wins! Rock beats Scissors!");
-        }
-        else
-        {
-            console.log("The Player wins! Scissors beats Paper!");
-        }
-        
-    }
-
 }
 
-const playerSelection = getPlayerChoice();
-console.log(playerSelection);
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+
+
 
