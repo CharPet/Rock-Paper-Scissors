d53b1db4de416955e8115344cf127aa8a46b6465
Chameleon <chameleon@Xubuntu.myguest.virtualbox.org>
2023-07-20 13:54:56 +0300

    Added Player Prompt and lower case conversion

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function getComputerChoice()
 {
-    const choices = ["Rock", "Paper" , "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 } 
 
 const randomChoice = getComputerChoice();
-console.log(randomChoice);
+// console.log(randomChoice);
+
+function aRound()
+{
+    let playerSelection = prompt("Make a choice:", "Rock, Paper, Scissors");
+    if (playerSelection !== null)
+    {
+        return playerSelection.toLowerCase();
+    }
+
+}
+
+const playerChoice = aRound();
+console.log(playerChoice);
